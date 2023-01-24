1a9a17b4e8eca1f8ee1754dd7b8b539adb79bc19
prandeveloper <pranaydevelopersveltosest@gmail.com>
2023-01-24 17:54:57 +0530

    24/01/2023 pran

--- src/views/apps/freshlist/customer/EditCustomer.js
+++ src/views/apps/freshlist/customer/EditCustomer.js
@@ -5,9 +5,12 @@ import {
   Col,
   Button,
   Table,
-  Label,
   Input,
+  Label,
+  Form,
+  FormGroup,
   Container,
+  CustomInput,
 } from "reactstrap";
 import { Trash2, Edit } from "react-feather";
 import axiosConfig from "../../../../axiosConfig";
@@ -21,27 +24,99 @@ class EditCustomer extends React.Component {
     super(props);
     this.state = {
       data: {},
+      customer_name: "",
+      email: "",
+      phone: "",
+      status: "",
+      selectedFile: null,
+      selectedName: "",
     };
   }
+
+  onChangeHandler = event => {
+    this.setState({ selectedFile: event.target.files[0] });
+    this.setState({ selectedName: event.target.files[0].name });
+    console.log(event.target.files[0]);
+  };
+  changeHandler = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleChange = e => {
+    this.setState({ status: e.target.value });
+  };
+
   componentDidMount() {
     // console.log(this.props.match.params);
     let { id } = this.props.match.params;
     axiosConfig
-      .get(`/admin/viewone_order/${id}`)
-      .then((response) => {
+      .get(`/user/getviewone/${id}`)
+      .then(response => {
         console.log(response.data.data);
-        this.setState({ data: response.data.data });
+        this.setState({
+          data: response.data.data,
+          customer_name: response.data.data.username,
+          email: response.data.data.email,
+          phone: response.data.data.mobile,
+        });
       })
-      .catch((error) => {
+      .catch(error => {
         console.log(error);
       });
   }
+
+  submitHandler = e => {
+    e.preventDefault();
+    console.log(this.props.match.params, this.state);
+    const data = new FormData();
+    data.append("username", this.state.customer_name);
+    data.append("email", this.state.email);
+    data.append("mobile", this.state.phone);
+    data.append("image", this.state.selectedFile, this.state.selectedName);
+    for (var value of data.values()) {
+      console.log(value);
+    }
+    for (var key of data.keys()) {
+      console.log(key);
+    }
+    let { id } = this.props.match.params;
+    axiosConfig
+      .post(`/user/edituser/${id}`, data)
+      .then(response => {
+        console.log(response);
+        this.props.history.push("/app/freshlist/customer/editCustomer");
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   render() {
     return (
       <React.Fragment>
         <Container>
+          <Row className="m-2">
+            <Col>
+              <h1 col-sm-6 className="float-left">
+                Edit Customer
+              </h1>
+            </Col>
+            <Col>
+              <Route
+                render={({ history }) => (
+                  <Button
+                    className=" btn btn-danger float-right"
+                    onClick={() =>
+                      history.push("/app/freshlist/customer/customerList")
+                    }
+                  >
+                    Back
+                  </Button>
+                )}
+              />
+            </Col>
+          </Row>
           <Row>
-            <Col sm="9">
+            {/* <Col sm="9">
               <Card>
                 <Row className="m-2">
                   <Col sm="6">
@@ -49,10 +124,10 @@ class EditCustomer extends React.Component {
                       <b>Order ID</b>
                       <span> #45112</span>
                     </h4>
-                    {/* <p className="my-1">
+                    <p className="my-1">
                       <img src={imagecal} alt="cl" width={15} /> 05 Jan 2023
                       01:33:44
-                    </p> */}
+                    </p>
                     <p>Total KM: 20KM</p>
                     <p>Total Time :27MIN</p>
                   </Col>
@@ -84,11 +159,11 @@ class EditCustomer extends React.Component {
                                   className="mr-50"
                                   size="25px"
                                   color="blue"
-                                  // onClick={() =>
-                                  //   history.push(
-                                  //     `/app/freshlist/order/editOrder/${params.data._id}`
-                                  //   )
-                                  // }
+                                  onClick={() =>
+                                    history.push(
+                                      `/app/freshlist/order/editOrder/${params.data._id}`
+                                    )
+                                  }
                                 />
                               )}
                             />
@@ -98,14 +173,14 @@ class EditCustomer extends React.Component {
                                   className="mr-50"
                                   size="25px"
                                   color="red"
-                                  // onClick={() => {
-                                  //   let selectedData =
-                                  //     this.gridApi.getSelectedRows();
-                                  //   this.runthisfunction(params.data._id);
-                                  //   this.gridApi.updateRowData({
-                                  //     remove: selectedData,
-                                  //   });
-                                  // }}
+                                  onClick={() => {
+                                    let selectedData =
+                                      this.gridApi.getSelectedRows();
+                                    this.runthisfunction(params.data._id);
+                                    this.gridApi.updateRowData({
+                                      remove: selectedData,
+                                    });
+                                  }}
                                 />
                               )}
                             />
@@ -123,11 +198,11 @@ class EditCustomer extends React.Component {
                                   className="mr-50"
                                   size="25px"
                                   color="blue"
-                                  // onClick={() =>
-                                  //   history.push(
-                                  //     `/app/freshlist/order/editOrder/${params.data._id}`
-                                  //   )
-                                  // }
+                                  onClick={() =>
+                                    history.push(
+                                      `/app/freshlist/order/editOrder/${params.data._id}`
+                                    )
+                                  }
                                 />
                               )}
                             />
@@ -137,14 +212,14 @@ class EditCustomer extends React.Component {
                                   className="mr-50"
                                   size="25px"
                                   color="red"
-                                  // onClick={() => {
-                                  //   let selectedData =
-                                  //     this.gridApi.getSelectedRows();
-                                  //   this.runthisfunction(params.data._id);
-                                  //   this.gridApi.updateRowData({
-                                  //     remove: selectedData,
-                                  //   });
-                                  // }}
+                                  onClick={() => {
+                                    let selectedData =
+                                      this.gridApi.getSelectedRows();
+                                    this.runthisfunction(params.data._id);
+                                    this.gridApi.updateRowData({
+                                      remove: selectedData,
+                                    });
+                                  }}
                                 />
                               )}
                             />
@@ -155,33 +230,139 @@ class EditCustomer extends React.Component {
                   </Row>
                 </Container>
               </Card>
-            </Col>
-            <Col sm="3">
+            </Col> */}
+            <Col sm="12">
               <Card>
                 <Row className="m-2">
                   <Col sm="12" className="my-1">
-                    <Label>Customer Information</Label>
-                    <Row>
-                      <Col sm="6">
+                    <h3>Customer Information</h3>
+                    <Row className="m-3">
+                      <Col sm="4">
                         <img
-                          src="https://img.freepik.com/premium-vector/cute-little-boy-cartoon-waving-hand_322598-79.jpg?w=740"
+                          src={this.state.data?.image}
                           alt="aaa"
-                          width="50"
-                          height="80"
+                          width="200"
+                          height="150"
                         />
                       </Col>
-                      <Col sm="6">
-                        <h6>
-                          <b>Rahul Sharma</b>
-                        </h6>
-                        <p>
-                          <b>11</b>Orders
-                        </p>
-                        <p>#100210</p>
-                        <p>customer@customer.com</p>
+                      <Col sm="8">
+                        <Row className="m-2">
+                          <Col>
+                            <h6>UserName</h6>
+                          </Col>
+                          <Col>
+                            <h6>
+                              <b>{this.state.data?.username}</b>
+                            </h6>
+                          </Col>
+                        </Row>
+                        <Row className="m-2">
+                          <Col>
+                            <h6>Email</h6>
+                          </Col>
+                          <Col>
+                            <h6>
+                              <b>{this.state.data?.email}</b>
+                            </h6>
+                          </Col>
+                        </Row>
+                        <Row className="m-2">
+                          <Col>
+                            <h6>Phone</h6>
+                          </Col>
+                          <Col>
+                            <h6>
+                              <b>{this.state.data?.mobile}</b>
+                            </h6>
+                          </Col>
+                        </Row>
                       </Col>
                     </Row>
                   </Col>
+                </Row>
+                <Row className="m-2">
+                  <Form className="m-1" onSubmit={this.submitHandler}>
+                    <Row className="mb-2">
+                      <Col lg="6" md="6">
+                        <FormGroup>
+                          <Label>Customer Name</Label>
+                          <Input
+                            type="text"
+                            placeholder="Customer_Name"
+                            name="customer_name"
+                            value={this.state.customer_name}
+                            onChange={this.changeHandler}
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col lg="6" md="6">
+                        <FormGroup>
+                          <Label>Email</Label>
+                          <Input
+                            type="email"
+                            placeholder="Enter Email"
+                            name="email"
+                            value={this.state.email}
+                            onChange={this.changeHandler}
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col lg="6" md="6">
+                        <FormGroup>
+                          <Label>Mobile No.</Label>
+                          <Input
+                            type="Number"
+                            placeholder="Enter No."
+                            name="phone"
+                            value={this.state.phone}
+                            onChange={this.changeHandler}
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col lg="6" md="6">
+                        <Label>Image</Label>
+                        <FormGroup>
+                          <CustomInput
+                            type="file"
+                            onChange={this.onChangeHandler}
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col lg="6" md="6" sm="6" className="mb-2 mt-1">
+                        <Label className="mb-1">Status</Label>
+                        <div
+                          className="form-label-group"
+                          onChange={this.handleChange}
+                        >
+                          <input
+                            style={{ marginRight: "3px" }}
+                            type="radio"
+                            name="status"
+                            value="true"
+                          />
+                          <span style={{ marginRight: "20px" }}>True</span>
+
+                          <input
+                            style={{ marginRight: "3px" }}
+                            type="radio"
+                            name="status"
+                            value="false"
+                          />
+                          <span style={{ marginRight: "3px" }}>False</span>
+                        </div>
+                      </Col>
+                    </Row>
+
+                    <Row className="m-2">
+                      <Button
+                        color="primary"
+                        type="submit"
+                        className="mr-1 mb-1"
+                      >
+                        Add Customer
+                      </Button>
+                    </Row>
+                  </Form>
                 </Row>
               </Card>
             </Col>
